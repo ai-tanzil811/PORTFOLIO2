b67fb01eba826c15239b3078533f7e2491b1742d
ai-tanzil811 <atanzil223028@bscse.uiu.ac.bd>
2025-07-22 03:50:01 +0600

    Refactor code structure for improved readability and maintainability

--- main.js
+++ main.js
@@ -1,228 +1,32 @@
-// Character data structure
-const characters = [
-    {
-        id: "omen",
-        name: "Omen",
-        role: "Controlador",
-        roleEng: "CONTROLLER",
-        background: "https://images4.alphacoders.com/131/1319447.jpeg",
-        portrait: "https://i.namu.wiki/i/6zCYHawLrCQRwVU73XyB-3UnMTkoJ2XnDa5BETMWPv5gT3WTutTZUIOXczvscHmg2GpfnW6jUfqB4RyS9p7ReuLf9OZuDnZDzQptsZtnQdhI06b1GH_inpBLQZqxfwuQEPy9fXMV-eHLh1kmFphdVA.webp",
-        icon: "https://gamerhub.gg/images/source/a30b236eb6904c37baff000c8e08a9c9.png",
-        description: "A phantom of a memory, Omen hunts in the shadows. He renders enemies blind, teleports across the field, then lets paranoia take hold as his foe scrambles to learn where he might strike next.",
-        abilities: [
-            {key: "q", icon: "https://titles.trackercdn.com/valorant-api/agents/8e253930-4c05-31dd-1b6c-968525494517/abilities/ability1/displayicon.png"},
-            {key: "e", icon: "https://titles.trackercdn.com/valorant-api/agents/8e253930-4c05-31dd-1b6c-968525494517/abilities/ability2/displayicon.png"},
-            {key: "c", icon: "https://titles.trackercdn.com/valorant-api/agents/8e253930-4c05-31dd-1b6c-968525494517/abilities/grenade/displayicon.png"},
-            {key: "x", icon: "https://titles.trackercdn.com/valorant-api/agents/8e253930-4c05-31dd-1b6c-968525494517/abilities/ultimate/displayicon.png"}
-        ],
-        displayIcon: "https://titles.trackercdn.com/valorant-api/agents/8e253930-4c05-31dd-1b6c-968525494517/displayicon.png",
-        buttonBg: "https://www.yudiz.com/codepen/valorant-characters/button-bg.svg",
-        colorClass: "banner-loop-one"
-    },
-    {
-        id: "phoenix",
-        name: "Phoenix",
-        role: "duelist",
-        roleEng: "CONTROLLER",
-        background: "https://www.pixground.com/wp-content/uploads/2023/08/Agent-Phoenix-Valorant-4K-Wallpaper-1-1081x608.jpg",
-        portrait: "https://trackercdn.com/cdn/tracker.gg/valorant/db/agents/phoenix_portrait.png",
-        icon: "https://valorantinfo.gg/wp-content/uploads/2021/10/valorant-spotlight-spray.png",
-        description: "Hailing from the U.K., Phoenix's star power shines through in his fighting style, igniting the battlefield with flash and flare. Whether he's got backup or not, he's rushing in to fight on his own terms",
-        abilities: [
-            {key: "q", icon: "https://titles.trackercdn.com/valorant-api/agents/eb93336a-449b-9c1b-0a54-a891f7921d69/abilities/ability1/displayicon.png"},
-            {key: "e", icon: "https://titles.trackercdn.com/valorant-api/agents/eb93336a-449b-9c1b-0a54-a891f7921d69/abilities/ability2/displayicon.png"},
-            {key: "c", icon: "https://titles.trackercdn.com/valorant-api/agents/eb93336a-449b-9c1b-0a54-a891f7921d69/abilities/grenade/displayicon.png"},
-            {key: "x", icon: "https://titles.trackercdn.com/valorant-api/agents/eb93336a-449b-9c1b-0a54-a891f7921d69/abilities/ultimate/displayicon.png"}
-        ],
-        displayIcon: "https://titles.trackercdn.com/valorant-api/agents/eb93336a-449b-9c1b-0a54-a891f7921d69/displayicon.png",
-        buttonBg: "https://www.yudiz.com/codepen/valorant-characters/button-bg-2.svg",
-        colorClass: "banner-loop-second"
-    },
-    {
-        id: "viper",
-        name: "VIper",
-        role: "Controlador",
-        roleEng: "CONTROLLER",
-        background: "https://images.hdqwalls.com/wallpapers/viper-valorant-2020-4k-4g.jpg",
-        portrait: "https://trackercdn.com/cdn/tracker.gg/valorant/db/agents/viper_portrait.png",
-        icon: "https://titles.trackercdn.com/valorant-api/agents/707eab51-4836-f488-046a-cda6bf494859/displayicon.png",
-        description: "The American Chemist, Viper deploys an array of poisonous chemical devices to control the battlefield and choke the enemy's vision. If the toxins don't kill her prey, her mindgames surely will..",
-        abilities: [
-            {key: "q", icon: "https://titles.trackercdn.com/valorant-api/agents/707eab51-4836-f488-046a-cda6bf494859/abilities/ability1/displayicon.png"},
-            {key: "e", icon: "https://titles.trackercdn.com/valorant-api/agents/707eab51-4836-f488-046a-cda6bf494859/abilities/ability2/displayicon.png"},
-            {key: "c", icon: "https://titles.trackercdn.com/valorant-api/agents/707eab51-4836-f488-046a-cda6bf494859/abilities/grenade/displayicon.png"},
-            {key: "x", icon: "https://titles.trackercdn.com/valorant-api/agents/707eab51-4836-f488-046a-cda6bf494859/abilities/ultimate/displayicon.png"}
-        ],
-        displayIcon: "https://titles.trackercdn.com/valorant-api/agents/707eab51-4836-f488-046a-cda6bf494859/displayicon.png",
-        buttonBg: "https://www.yudiz.com/codepen/valorant-characters/button-bg-3.svg",
-        colorClass: "banner-loop-third"
-    }
-];
-
-jQuery(document).ready(function() {
-    // Generate HTML for characters
-    generateCharactersHTML();
-    
-    // Initialize parallax effect
+jQuery(document).ready(function () {
+    // Initialize parallax effect for profile image
     initParallax();
     
-    // Initialize sliders
-    initSliders();
-    
-    // Custom Mouse Pointer 
+    // Initialize custom cursor
     initCustomCursor();
     
-    // Make website responsive to touch events
-    initTouchEvents();
+    // Add animation to main image
+    setTimeout(function() {
+        document.querySelector(".main-img").classList.add("character-animation");
+    }, 500);
     
-    // Add resize event listener for responsive adjustments
+    // Init modal functionality
+    initModals();
+    
+    // Add resize listener
     window.addEventListener('resize', handleResize);
     
     // Initial resize check
     handleResize();
-    
-    // Ensure rain effect is generated for each slide
-    jQuery('.banner-section-inner').on('afterChange', function() {
-        // If rain.js defines a global function, call it here
-        if (typeof generateRain === 'function') {
-            generateRain();
-        }
-    });
 });
-
-function generateCharactersHTML() {
-    // Clear existing content
-    jQuery('.banner-section-inner').empty();
-    jQuery('.controller-right-icons-inner').empty();
-    
-    // Generate HTML for each character
-    characters.forEach(character => {
-        // Create main character slide
-        const characterSlide = `
-        <div class="banner-section-loop ${character.colorClass}" style="background-image: url('${character.background}')">
-          <div class="banner-left-vertical-main">
-            <h6>BIND PERSONALIZADA</h6>
-            <img src="${character.icon}" class="gl-logo-img"/>  
-          </div>
-          <div class="banner-main-img">
-            <div class="main-img scene">
-              <img src="${character.portrait}" alt="${character.name}" class="layer img-responsive" data-depth="0.2" />
-            </div>
-          </div>
-          <div class="banner-content-main">
-            <h5>${character.role}</h5>
-            <h1>${character.name}</h1>
-            <div class="controller-box-section">
-              <div class="controller-box-img">
-                <img src="${character.icon}" alt="controller-img">
-              </div>
-              <div class="controller-box-content">
-                <h5>${character.roleEng}</h5>
-                <p>${character.description}</p>
-              </div>
-            </div>
-            <div class="controller-icons-main">
-              ${character.abilities.map(ability => `
-                <div class="controller-icons-inner">
-                  <div class="controller-img">
-                    <img src="${ability.icon}" alt="">
-                  </div>
-                  <p>${ability.key}</p>
-                </div>
-              `).join('')}
-            </div>
-            <div class="view-contract-btn-main cursor-scale" style="background-image: url('${character.buttonBg}')">
-              <a href="#" class="view-contract-btn">view contract</a>
-            </div>
-          </div>
-        </div>
-        `;
-        
-        // Add character slide to the container
-        jQuery('.banner-section-inner').append(characterSlide);
-        
-        // Add character icon to the sidebar
-        jQuery('.controller-right-icons-inner').append(`
-            <div>
-                <img src="${character.displayIcon}" alt="${character.name}" />
-            </div>
-        `);
-    });
-}
 
 function initParallax() {
     var scene = document.querySelectorAll(".scene");
-    scene.forEach(function(el) {
-        var parallax = new Parallax(el);
-    });
-}
-
-function initSliders() {
-    jQuery('.banner-section-inner').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: false,
-        fade: true,
-        speed: 800,
-        asNavFor: '.controller-right-icons-inner',
-        touchThreshold: 100,
-        adaptiveHeight: true,
-        responsive: [
-            {
-                breakpoint: 767,
-                settings: {
-                    dots: true,
-                    adaptiveHeight: true
-                }
-            }
-        ]
-    });
-    
-    jQuery('.controller-right-icons-inner').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.banner-section-inner',
-        arrows: false,
-        dots: false,
-        focusOnSelect: true,
-        variableWidth: true,
-        responsive: [
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 3,
-                    centerMode: true
-                }
-            }
-        ]
-    });
-    
-    jQuery('.banner-section-inner').on('afterChange', function(event, slick, currentSlide) {
-        updateHeaderClass();
-        animateCharacter();
-    });
-    
-    // Initial update
-    updateHeaderClass();
-}
-
-function updateHeaderClass() {
-    if (jQuery(".banner-loop-one").hasClass('slick-current')) {
-        jQuery(".header-section-main").removeClass("header-section-orange header-section-green").addClass("header-section-blue");
-    } else if (jQuery(".banner-loop-second").hasClass('slick-current')) {
-        jQuery(".header-section-main").removeClass("header-section-blue header-section-green").addClass("header-section-orange");
-    } else if (jQuery(".banner-loop-third").hasClass('slick-current')) {
-        jQuery(".header-section-main").removeClass("header-section-blue header-section-orange").addClass("header-section-green");
+    if (scene.length > 0) {
+        scene.forEach(function(el) {
+            var parallax = new Parallax(el);
+        });
     }
-}
-
-function animateCharacter() {
-    jQuery(".banner-main-img .main-img").removeClass("character-animation");
-    setTimeout(function() {
-        jQuery(".banner-main-img .main-img").addClass("character-animation");
-    }, 50);
 }
 
 function initCustomCursor() {
@@ -234,12 +38,14 @@ function initCustomCursor() {
     gsap.to({}, 0.016, {
         repeat: -1,
         onRepeat: function() {
-            gsap.set(cursor, {
-                css: {
-                    left: mouseX,
-                    top: mouseY,
-                }
-            });
+            if (cursor) {
+                gsap.set(cursor, {
+                    css: {
+                        left: mouseX,
+                        top: mouseY,
+                    }
+                });
+            }
         }
     });
     
@@ -248,43 +54,155 @@ function initCustomCursor() {
         mouseY = e.clientY;
     });
     
-    cursorScale.forEach(link => {
-        link.addEventListener('mousemove', () => {
-            cursor.classList.add('grow');
-            if (link.classList.contains('small')) {
+    if (cursorScale) {
+        cursorScale.forEach(link => {
+            link.addEventListener('mousemove', () => {
+                cursor.classList.add('grow');
+                if (link.classList.contains('small')) {
+                    cursor.classList.remove('grow');
+                    cursor.classList.add('grow-small');
+                }
+            });
+            
+            link.addEventListener('mouseleave', () => {
                 cursor.classList.remove('grow');
-                cursor.classList.add('grow-small');
-            }
+                cursor.classList.remove('grow-small');
+            });
         });
-        
-        link.addEventListener('mouseleave', () => {
-            cursor.classList.remove('grow');
-            cursor.classList.remove('grow-small');
+    }
+}
+
+
+
+function initSmoothScroll() {
+    // Smooth scroll for navigation links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            
+            const targetId = this.getAttribute('href');
+            if (targetId === '#') return;
+            
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 100,
+                    behavior: 'smooth'
+                });
+            }
         });
     });
 }
 
-function initTouchEvents() {
-    // Add swipe support for mobile
-    jQuery('.banner-section-inner').on('swipe', function(event, slick, direction) {
-        if (direction === 'left') {
-            jQuery(this).slick('slickNext');
+function initScrollEffects() {
+    window.addEventListener('scroll', function() {
+        const scrollPosition = window.scrollY;
+        const header = document.querySelector('.header-section-main');
+        
+        if (scrollPosition > 100) {
+            header.style.background = 'rgba(13, 2, 33, 0.95)';
+            header.style.boxShadow = '0 0 20px rgba(0, 255, 65, 0.5)';
         } else {
-            jQuery(this).slick('slickPrev');
+            header.style.background = 'rgba(13, 2, 33, 0.8)';
+            header.style.boxShadow = '0 0 20px rgba(0, 255, 65, 0.3)';
+        }
+        
+        // Check which section is visible and highlight nav item
+        const sections = document.querySelectorAll('section[id]');
+        
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - 150;
+            const sectionHeight = section.offsetHeight;
+            const sectionId = section.getAttribute('id');
+            
+            if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+                document.querySelector(`.header-menu ul li a[href="#${sectionId}"]`)?.parentElement?.classList.add('active');
+            } else {
+                document.querySelector(`.header-menu ul li a[href="#${sectionId}"]`)?.parentElement?.classList.remove('active');
+            }
+        });
+    });
+}
+
+// Fix for modal initialization
+function initModals() {
+    // Open modal when clicking on menu items
+    document.querySelectorAll('[data-modal]').forEach(trigger => {
+        trigger.addEventListener('click', function(e) {
+            e.preventDefault();
+            const modalId = this.getAttribute('data-modal');
+            const modal = document.getElementById(modalId);
+            if (modal) {
+                openModal(modal);
+            }
+        });
+    });
+    
+    // Close modal when clicking on close button
+    document.querySelectorAll('.modal-close').forEach(closeBtn => {
+        closeBtn.addEventListener('click', function() {
+            const modal = this.closest('.modal-overlay');
+            closeModal(modal);
+        });
+    });
+    
+    // Close modal when clicking outside modal content
+    document.querySelectorAll('.modal-overlay').forEach(overlay => {
+        overlay.addEventListener('click', function(e) {
+            if (e.target === this) {
+                closeModal(this);
+            }
+        });
+    });
+    
+    // Close modal with ESC key
+    document.addEventListener('keydown', function(e) {
+        if (e.key === 'Escape') {
+            const activeModal = document.querySelector('.modal-overlay.active');
+            if (activeModal) {
+                closeModal(activeModal);
+            }
         }
     });
 }
 
-function handleResize() {
-    const width = window.innerWidth;
-    
-    // Toggle classes or adjust elements based on screen size
-    if (width <= 767) {
-        jQuery('.banner-section-inner').slick('slickSetOption', 'dots', true, true);
-    } else {
-        jQuery('.banner-section-inner').slick('slickSetOption', 'dots', false, true);
+function openModal(modal) {
+    if (modal) {
+        // Reset scroll position
+        modal.querySelector('.modal-content').scrollTop = 0;
+        
+        // Add active class to show modal with animation
+        modal.classList.add('active');
+        
+        // Prevent body scrolling
+        document.body.style.overflow = 'hidden';
     }
-    
+}
+
+function closeModal(modal) {
+    if (modal) {
+        // Remove active class to hide modal with animation
+        modal.classList.remove('active');
+        
+        // Re-enable body scrolling
+        document.body.style.overflow = '';
+    }
+}
+
+function handleResize() {
     // Reinitialize parallax on resize
     setTimeout(initParallax, 100);
+    
+    // Adjust layout for mobile
+    if (window.innerWidth <= 768) {
+        // Mobile-specific adjustments
+    }
 }
+
+// Add this at the bottom of the file to ensure all elements are properly initialized
+document.addEventListener('DOMContentLoaded', function() {
+    // Re-initialize modals after a slight delay to ensure all elements are ready
+    setTimeout(function() {
+        initModals();
+    }, 100);
+});
